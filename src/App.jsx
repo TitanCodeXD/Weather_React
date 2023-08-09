fe0338753415abf96d1ec04ea25f52de29513f41
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-08-08 21:57:43 -0300

    Finalizando os estados da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import {useState} from 'react';
 import './App.css';
 
 function App() {
-const [city, setCity] = useState("aa");
+const [city, setCity] = useState("");
+const  [weatherForecast, setWeatherForecast] = useState(null)
 
 const handleChange = (event) => {
 setCity(event.target.value);
+};
+
+const handleSearch = () => {
+  fetch(`http://api.weatherapi.com/v1/current.json?key=e29a92e1e65d4405bae231117230808&q=${city}&lang=pt`
+  ).then((response) => {
+    if (response.status === 200) {
+      return response.json()
+    }
+  }
+  ).then((data) => {
+    setWeatherForecast(data);
+  });
 };
 
   return (
@@ -38,9 +51,28 @@ setCity(event.target.value);
               </div>
             </div>
 
-            <button className = "btn btn-primary btn-lg">
+            <button onClick= {handleSearch} className = "btn btn-primary btn-lg">
               Pesquisar
             </button>
+
+
+          {
+            weatherForecast ? (
+              <div>
+              <div className = "mt-4 d-flex align-items-center">
+
+                <div>
+                  <img src = {weatherForecast.current.condition.icon}></img>
+                </div>
+
+                <div>
+                  <h3>Hoje o dia está: {weatherForecast.current.condition.text}</h3>
+                  <p>Temp: {weatherForecast.current.temp_c}°</p>
+                </div>
+
+                </div>
+            </div>
+          ): null}
 
           </div>
 
