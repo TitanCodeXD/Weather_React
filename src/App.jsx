a91510e168eef023d42a024f4a29e63211016672
TitanCodeXD <wesleysantos32892653@outlook.com>
2023-08-08 21:12:00 -0300

    fazendo a interface inicial e aplicando useState ao input de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import './App.css'
+import {useState} from 'react';
+import './App.css';
 
 function App() {
+const [city, setCity] = useState("aa");
 
+const handleChange = (event) => {
+setCity(event.target.value);
+};
 
   return (
     <>
       <div>
-        <nav>
-        Teste
+
+
+        <nav className = "navbar navbar-expanded-md navbar-dark bg-dark mb-4">
+        <a className = "navbar-brand text-white ml-4" href = "#">Previsão do Tempo</a>
         </nav>
+
+
+        <main className = "container">
+          <div className = "jumbotron">
+            <h1>
+              Verifique a previsão do tempo da sua cidade!
+            </h1>
+            <p className = "lead">
+              Digite o nome da cidade e em seguida clique em pesquisar :)
+            </p>
+
+            <div className = "row mb-4">
+              <div className = "col-md-6">
+                <input 
+                onChange={handleChange}
+                className = "form-control" 
+                value = {city}>
+
+                </input>
+              </div>
+            </div>
+
+            <button className = "btn btn-primary btn-lg">
+              Pesquisar
+            </button>
+
+          </div>
+
+        </main>
+
       </div>
     </>
   )
